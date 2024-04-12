0e06ba0a3ed561dc8b640982437bc2a785f5428d
Bon-Image <bonimage@pm.me>
2024-04-12 14:06:15 +0200

    Added the event listener to the container, set up the event handler function and removed unused elements from the styles.css file.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 const body = document.querySelector("body");
 const container = document.querySelector("#container");
-for (j = 0; j <= 5; j++) {
+for (j = 0; j <= 50; j++) {
     let row = document.createElement("div")
     row.classList.add("r");
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 50; i++) {
         let squar = document.createElement("div");
         squar.classList.add("s");
         row.appendChild(squar);
     }
     container.appendChild(row);
+
 }
+const changeColor = function (event) {
+    if (event.target.classList.contains("s")){
+        event.target.style.backgroundColor = "green"; 
+    }
+
+}
+
+container.addEventListener("mouseout", changeColor)
+
+
+
+
+
+
+
+/* const squars = document.getElementsByClassName("s");
+squars.addEventListener("mouseleave", () => {
+    console.log("Test")
+}) */
