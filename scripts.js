e921600a22d2fb14e23f72fd375c2778bf0ec973
Bon-Image <bonimage@pm.me>
2024-04-15 12:40:51 +0200

    Finished the first version of the page. removed the container div from html, created an initialize function to create the grid in js and removed the height and width from the css and coded those in the js.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,68 @@
-const body = document.querySelector("body");
-const container = document.querySelector("#container");
-for (j = 0; j <= 50; j++) {
-    let row = document.createElement("div")
-    row.classList.add("r");
-    for (let i = 0; i <= 50; i++) {
-        let squar = document.createElement("div");
-        squar.classList.add("s");
-        row.appendChild(squar);
+initialize()
+
+function initialize(numberSquares = 16) {
+
+    const body = document.querySelector("body");
+    const container = document.createElement("div");
+    container.setAttribute("id", "container");
+    body.appendChild(container);
+
+    makeGrid(numberSquares) // Creates a Default sketch pad, before users insert thier desirable size. 
+
+    const sideLength = 500 / numberSquares; // Side length of each squar. 
+    console.log(sideLength)
+    let squares = document.getElementsByClassName("s");
+
+    for (i = 0; i < squares.length; i++) {
+
+        squares[i].style.height = `${sideLength}px`;
+        squares[i].style.width = `${sideLength}px`;
+
     }
-    container.appendChild(row);
+
+
+    // Add an Event Listener to the main container, we are doing Event-Delegation.
+    container.addEventListener("mouseout", changeColor)
 
 }
-const changeColor = function (event) {
-    if (event.target.classList.contains("s")){
-        event.target.style.backgroundColor = "green"; 
+
+function makeGrid(numberSquares = 16) {
+
+    // Because we need horizontal containers to accomodate square cells. There are rows in out grid. 
+    for (j = 0; j < numberSquares; j++) {
+        let row = document.createElement("div")
+        row.classList.add("r");
+
+        // Creation of a single square and appending it to the current horizontal container.  
+        for (let i = 0; i < numberSquares; i++) {
+            let squar = document.createElement("div");
+            squar.classList.add("s");
+            row.appendChild(squar);
+        }
+
+        // We filled the current row with squares, now we have to do the same again for the rest. 
+        container.appendChild(row);
     }
-
 }
-container.addEventListener("mouseout", changeColor)
 
+// After mouse leavse each squar, we change the color of that square. 
+function changeColor(event) {
 
+    if (event.target.classList.contains("s")) {
+        event.target.style.backgroundColor = "green";
+    }
+}
 
+// We give the user a chance to start the sketch. 
+const size = document.querySelector("button");
+size.textContent = "Choose the Sketch Size Please"
 
+size.addEventListener("click", () => {
+    numberSquares = prompt("Enter the number of squares per side: ")
+    removeGrid();
+    initialize(numberSquares)
+})
 
-
-
-/* const squars = document.getElementsByClassName("s");
-squars.addEventListener("mouseleave", () => {
-    console.log("Test")
-}) */
+function removeGrid() {
+    container.remove()
+}
